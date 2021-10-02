0c7233725b46a9487ef56109d81e108939a823c4
Ali <armib20@gmail.com>
2021-10-01 22:11:22 -0400

    Optimized
    Replaced arrays with variables

--- PiEstimator/PiEstimator/Program.cs
+++ PiEstimator/PiEstimator/Program.cs
@@ -26,16 +26,16 @@ namespace PiEstimator
             Random rand = new Random(System.Environment.TickCount);
             double count = 0.0;
             double pi = 0.0;
-            double[] x_vals = new double[n];
-            double[] y_vals = new double[n];
-            
+            double x;
+            double y;
+
             // TODO: Calculate Pi
 
             for (int i = 0; i < n; i++)
             {
-                x_vals[i] = rand.NextDouble();
-                y_vals[i] = rand.NextDouble();
-                if ((x_vals[i] * x_vals[i]) + (y_vals[i] * y_vals[i]) <= 1)
+                x = rand.NextDouble();
+                y = rand.NextDouble();
+                if ((x * x) + (y * y) <= 1)
                 {
                     count += 1;
                 }
